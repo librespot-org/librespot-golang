99b61b59dc09008a859133b7f0080168e2350fe1
Sean Purcell <s3purcell@gmail.com>
2016-01-13 23:41:52 -0800

    remove old code

--- shannon.go
+++ shannon.go
@@ -178,15 +178,3 @@ func (s *ShannonStream) RecvPacket() (cmd uint8, buf []byte, err error){
 	
 	return
 }
-
-
-
-
-// Reverse returns its argument string reversed rune-wise left to right.
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
-}
