45d19c780d09b14e0e8029a9bdcbb5e216bfddb8
Sean Purcell <s3purcell@gmail.com>
2016-06-18 00:06:47 -0700

    search suggest

--- examples/micro-controller/main.go
+++ examples/micro-controller/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"github.com/badfortrains/spotcontrol"
-	"strings"
-	"strconv"
-	"flag"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const defaultdevicename = "SpotControl"
 
-func chooseDevice(controller *spotcontrol.SpircController, reader *bufio.Reader) string{
+func chooseDevice(controller *spotcontrol.SpircController, reader *bufio.Reader) string {
 	devices := controller.ListDevices()
 	if len(devices) == 0 {
 		fmt.Println("no devices")
@@ -23,12 +23,12 @@ func chooseDevice(controller *spotcontrol.SpircController, reader *bufio.Reader)
 	for i, d := range devices {
 		fmt.Printf("%v) %v %v \n", i, d.Name, d.Ident)
 	}
-	
+
 	for {
 		fmt.Print("Enter device number: ")
 		text, _ := reader.ReadString('\n')
 		i, err := strconv.Atoi(strings.TrimSpace(text))
-		if err == nil && i < len(devices) && i >= 0{
+		if err == nil && i < len(devices) && i >= 0 {
 			return devices[i].Ident
 		}
 		fmt.Println("invalid device number")
@@ -36,7 +36,7 @@ func chooseDevice(controller *spotcontrol.SpircController, reader *bufio.Reader)
 	}
 }
 
-func getDevice(controller *spotcontrol.SpircController, ident string, reader *bufio.Reader) string{
+func getDevice(controller *spotcontrol.SpircController, ident string, reader *bufio.Reader) string {
 	if ident != "" {
 		return ident
 	} else {
@@ -59,7 +59,7 @@ func addMdns(controller *spotcontrol.SpircController, reader *bufio.Reader) {
 		fmt.Print("Enter device number: ")
 		text, _ := reader.ReadString('\n')
 		i, err := strconv.Atoi(strings.TrimSpace(text))
-		if err == nil && i < len(devices) && i >= 0{
+		if err == nil && i < len(devices) && i >= 0 {
 			url = devices[i].Url
 			break
 		}
@@ -70,7 +70,7 @@ func addMdns(controller *spotcontrol.SpircController, reader *bufio.Reader) {
 
 }
 
-func printHelp(){
+func printHelp() {
 	fmt.Println("\nAvailable commands:")
 	fmt.Println("load <track1> [...more tracks]: load tracks by spotify base 62 id")
 	fmt.Println("hello:                          ask devices to identify themselves")
@@ -81,7 +81,6 @@ func printHelp(){
 	fmt.Println("help:                           show this list\n")
 }
 
-
 func main() {
 	username := flag.String("username", "", "spotify username")
 	password := flag.String("password", "", "spotify password")
@@ -91,7 +90,7 @@ func main() {
 	flag.Parse()
 
 	var sController *spotcontrol.SpircController
-	if *username != "" && *password != ""{
+	if *username != "" && *password != "" {
 		sController = spotcontrol.Login(*username, *password, *appkey, *devicename)
 	} else if *blobPath != "" {
 		if _, err := os.Stat(*blobPath); os.IsNotExist(err) {
@@ -113,7 +112,7 @@ func main() {
 	for {
 		fmt.Print("Enter a command: ")
 		text, _ := reader.ReadString('\n')
-		cmds := strings.Split(strings.TrimSpace(text),  " ")
+		cmds := strings.Split(strings.TrimSpace(text), " ")
 
 		switch {
 		case cmds[0] == "load":
@@ -139,6 +138,10 @@ func main() {
 			addMdns(sController, reader)
 		case cmds[0] == "help":
 			printHelp()
+		case cmds[0] == "search" && len(cmds) > 1:
+			sController.Search(strings.Join(cmds[1:], " "))
+		case cmds[0] == "suggest" && len(cmds) > 1:
+			sController.Suggest(strings.Join(cmds[1:], " "))
 		}
 	}
 
---
+++ metadata.go
@@ -0,0 +1,135 @@
+package spotcontrol
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+)
+
+type Artist struct {
+	Image string `json:"image"`
+	Name  string `json:"name"`
+	Uri   string `json:"uri"`
+}
+
+type Album struct {
+	Artists []Artist `json:"artists"`
+	Image   string   `json:"image"`
+	Name    string   `json:"name"`
+	Uri     string   `json:"uri"`
+}
+
+type Track struct {
+	Albums     Album    `json:"album"`
+	Artists    []Artist `json:"artists"`
+	Image      string   `json:"image"`
+	Name       string   `json:"name"`
+	Uri        string   `json:"uri"`
+	Duration   int      `json:"duration"`
+	Popularity int      `json:"popularity"`
+}
+
+type TopHit struct {
+	Image string `json:"image"`
+	Name  string `json:"name"`
+	Uri   string `json:"uri"`
+	Log   struct {
+		Origin string `json:"origin"`
+		TopHit string `json:"top_hit"`
+	} `json:"log"`
+	Artists []Artist `json:"artists"`
+	Album   Album    `json:"album"`
+}
+
+type SearchResult struct {
+	Artists struct {
+		Hits  []Artist `json:"hits"`
+		Total int      `json:"total"`
+	} `json:"artists"`
+	Albums struct {
+		Hits  []Album `json:"hits"`
+		Total int     `json:"total"`
+	} `json:"albums"`
+	Tracks struct {
+		Hits  []Track `json:"hits"`
+		Total int     `json:"total"`
+	} `json:"tracks"`
+}
+
+func (c *SpircController) Search(search string) {
+	url := "hm://searchview/km/v2/search/" + url.QueryEscape(search) + "?limit=12&tracks-limit=100&catalogue=&country=US&locale=en&platform=zelda&username="
+
+	c.session.mercurySendRequest(mercuryRequest{
+		method:  "GET",
+		uri:     url,
+		payload: [][]byte{},
+	}, func(res mercuryResponse) {
+		result := &SearchResult{}
+		err := json.Unmarshal(res.combinePayload(), result)
+		if err != nil {
+			fmt.Println("err", err)
+		}
+
+		for _, a := range result.Artists.Hits {
+			fmt.Println(a.Name)
+		}
+	})
+}
+
+type SuggestResult struct {
+	Sections []struct {
+		RawItems json.RawMessage `json:"items"`
+		Typ      string          `json:"type"`
+	} `json:"sections"`
+	Albums  []Artist
+	Artists []Album
+	Tracks  []Track
+	TopHits []TopHit
+}
+
+func parseSuggest(body []byte) *SuggestResult {
+	result := &SuggestResult{}
+	err := json.Unmarshal(body, result)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	for _, s := range result.Sections {
+		switch s.Typ {
+		case "top-results":
+			err = json.Unmarshal(s.RawItems, &result.TopHits)
+		case "album-results":
+			err = json.Unmarshal(s.RawItems, &result.Albums)
+		case "artist-results":
+			err = json.Unmarshal(s.RawItems, &result.Artists)
+		case "track-results":
+			err = json.Unmarshal(s.RawItems, &result.Tracks)
+		}
+		if err != nil {
+			fmt.Println("err", err)
+		}
+	}
+	return result
+}
+
+func (res *mercuryResponse) combinePayload() []byte {
+	body := make([]byte, 0)
+	for _, p := range res.payload {
+		body = append(body, p...)
+	}
+	return body
+}
+
+func (c *SpircController) Suggest(search string) {
+	url := "hm://searchview/km/v3/suggest/" + url.QueryEscape(search) + "?limit=3&intent=2516516747764520149&sequence=0&catalogue=&country=&locale=&platform=zelda&username="
+
+	c.session.mercurySendRequest(mercuryRequest{
+		method:  "GET",
+		uri:     url,
+		payload: [][]byte{},
+	}, func(res mercuryResponse) {
+		result := parseSuggest(res.combinePayload())
+
+		fmt.Println(result.Artists)
+	})
+}
